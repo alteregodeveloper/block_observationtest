085480cab915bebfee788c244e72e8151a711edd
alterego developer <alteregodeveloper@outlook.com>
2019-10-07 19:29:38 -0500

    Block file is created

---
+++ block_observationsummarize.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @package   block_observationsummarize
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__).'/lib.php');
+
+class block_observationsummarize extends block_base {
+    public function init() {
+        $this->title = get_string('observationsummarize', 'block_observationsummarize');
+    }
+
+    function get_content() {
+        global $CFG, $OUTPUT;
+
+        if($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass;
+        $this->content->footer = '';
+
+        if (empty($this->instance)) {
+            $this->content->text   = '';
+            return $this->content;
+        }
+
+        $this->content->text  = 'Observation Summarize';
+
+        return $this->content;
+    }
+}
