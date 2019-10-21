fe69b6382b55028309db9875f746f307aebf8852
alterego developer <alteregodeveloper@outlook.com>
2019-10-21 14:10:17 -0500

    Mexico Spanish language file is added

---
+++ lang/es_mx/block_observationsummarize.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package   block_observationsummarize
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Resultados Test de Observación';
+$string['observationsummarize'] = 'Resultados de Observación';
+$string['modulenameplural'] = 'Resultados de Observación';
